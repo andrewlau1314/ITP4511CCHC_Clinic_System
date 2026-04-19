2b1103bb3084bdda117764ee2a6b52e566106399
andrewlau1314 <laujamly@gmail.com>
2026-04-19 13:07:52 +0800

    Initial commit

---
+++ src/main/java/com/cchc/cchc_clinic_system/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.cchc.cchc_clinic_system;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
